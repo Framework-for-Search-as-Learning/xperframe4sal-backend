feb8aaf5ca17b85cbd5bdc936f69eb1fe184f823
Alfredo Lucas <alfredoliu14@live.com>
2025-04-26 02:03:04 -0300

    change icf description to Optional on IcfDto

--- src/modules/icf2/dto/create-icf.dto.ts
+++ src/modules/icf2/dto/create-icf.dto.ts
@@ -1,5 +1,5 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty, IsString} from 'class-validator';
+import {IsNotEmpty, IsOptional, IsString} from 'class-validator';
 
 export class CreateIcfDto {
   @ApiProperty()
@@ -8,7 +8,7 @@ export class CreateIcfDto {
   title: string;
 
   @ApiProperty()
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   description: string;
 

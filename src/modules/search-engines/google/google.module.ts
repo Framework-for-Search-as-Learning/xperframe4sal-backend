71408da4f5953bf6811dc749c86c356b72e999e3
Alfredo Lucas <alfredoliu14@live.com>
2026-01-26 15:52:31 -0300

    fix: update GoogleModule to import Experiments2Module instead of Experiments2Service

--- src/modules/search-engines/google/google.module.ts
+++ src/modules/search-engines/google/google.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { GoogleController } from './google.controller';
 import { GoogleService } from './google.service';
 import { HttpModule } from 'src/modules/http/http.module';
-import { Experiments2Service } from 'src/modules/experiments2/experiments2.service';
+import { Experiments2Module } from 'src/modules/experiments2/experiments2.module';
 
 @Module({
-  imports: [HttpModule, Experiments2Service],
+  imports: [HttpModule, Experiments2Module],
   controllers: [GoogleController],
   providers: [GoogleService],
   exports: [GoogleService],

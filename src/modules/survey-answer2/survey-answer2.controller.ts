f57798d345358040e19fedf50fd1b861aeaa274c
Alfredo Lucas <alfredoliu14@live.com>
2026-02-09 16:30:02 -0300

    feat: add endpoint for retrieving survey answers by user and survey IDs

--- src/modules/survey-answer2/survey-answer2.controller.ts
+++ src/modules/survey-answer2/survey-answer2.controller.ts
@@ -1,12 +1,12 @@
-import {Body, Controller, Get, Param, Patch, Post, Query} from '@nestjs/common';
-import {SurveyAnswer2Service} from './survey-answer2.service';
-import {CreateSurveyAnswerDto} from './dto/create-surveyAnswer.dto';
-import {SurveyAnswer} from './entity/survey-answer.entity';
-import {UpdateSurveyAnswerDto} from './dto/update-surveyAnswer.dto';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { SurveyAnswer2Service } from './survey-answer2.service';
+import { CreateSurveyAnswerDto } from './dto/create-surveyAnswer.dto';
+import { SurveyAnswer } from './entity/survey-answer.entity';
+import { UpdateSurveyAnswerDto } from './dto/update-surveyAnswer.dto';
 
 @Controller('survey-answer2')
 export class SurveyAnswer2Controller {
-  constructor(private readonly surveyAnswerService: SurveyAnswer2Service) {}
+  constructor(private readonly surveyAnswerService: SurveyAnswer2Service) { }
 
   @Post()
   async create(
@@ -30,6 +30,17 @@ export class SurveyAnswer2Controller {
       return await this.surveyAnswerService.findByUserId(userId);
     }
     return await this.surveyAnswerService.findAll();
+  }
+
+  @Get('/user/:userId/survey/:surveyId')
+  async findByUserIdAndSurveyId(
+    @Param('userId') userId: string,
+    @Param('surveyId') surveyId: string,
+  ): Promise<SurveyAnswer> {
+    return await this.surveyAnswerService.findByUserIdAndSurveyId(
+      userId,
+      surveyId,
+    );
   }
 
   @Patch(':id')
